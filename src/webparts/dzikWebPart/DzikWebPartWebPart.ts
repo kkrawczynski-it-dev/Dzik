db5af2d13ad4984f3faf5af4084b8630d49695f4
kkrawczynski <kkrawczynski@it-dev.pl>
2018-09-05 08:44:09 +0200

    Initial commit

---
+++ src/webparts/dzikWebPart/DzikWebPartWebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './DzikWebPartWebPart.module.scss';
+import * as strings from 'DzikWebPartWebPartStrings';
+
+export interface IDzikWebPartWebPartProps {
+  description: string;
+}
+
+export default class DzikWebPartWebPart extends BaseClientSideWebPart<IDzikWebPartWebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${ styles.dzikWebPart }">
+        <div class="${ styles.container }">
+          <div class="${ styles.row }">
+            <div class="${ styles.column }">
+              <span class="${ styles.title }">Welcome to DzikPoint!</span>
+              <p class="${ styles.subTitle }">Styles origin - unknown</p>
+              <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <a href="https://aka.ms/spfx" class="${ styles.button }">
+                <span class="${ styles.label }">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/dzikWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDzikWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DzikWebPartWebPartStrings' {
+  const strings: IDzikWebPartWebPartStrings;
+  export = strings;
+}
